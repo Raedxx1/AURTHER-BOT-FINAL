a025b236f0d550802fce74a7c96de9db26a324ba
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-11-30 00:52:49 +0300

    Update game-guessFlag.js

--- plugins/game-guessFlag.js
+++ plugins/game-guessFlag.js
@@ -1,32 +1,30 @@
-import fetch from 'node-fetch'
-let timeout = 120000
-let poin = 4999
+let timeout = 60000
+let poin = 500
 let handler = async (m, { conn, command, usedPrefix }) => {
-    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    conn.tebaklagu = conn.tebaklagu ? conn.tebaklagu : {}
     let id = m.chat
-    if (id in conn.tebakbendera) {
-        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.tebakbendera[id][0])
+    if (id in conn.tebaklagu) {
+        conn.reply(m.chat, '*صبر ما تشوف فيه سؤال ؟*', conn.tebaklagu[id][0])
         throw false
     }
-    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakbendera2.json')).json()
+    let src = await (await fetch('https://raw.githubusercontent.com/kimos71/AURTER-BOT/main/Games/Flags.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*${command.toUpperCase()}*
-Timeout *${(timeout / 1000).toFixed(2)} second*
-use ${usedPrefix}fhint for hint
-Reward: ${poin} XP
-    `.trim()
-    conn.tebakbendera[id] = [
+    let caption = `*❃ ──────⊰ ❀ ⊱────── ❃*\n*ماهي الدولة ؟؟*\n
+  *الوقت :* *${(timeout / 1000).toFixed(2)}* *ثانية*
+   *الجائزة :* *${poin}* *بيلي*
+*❃ ──────⊰ ❀ ⊱────── ❃*
+     `.trim()
+    conn.tebaklagu[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `Time's up!\nThe answer is *${json.name}*`, conn.tebakbendera[id][0])
-            delete conn.tebakbendera[id]
+            if (conn.tebaklagu[id]) conn.reply(m.chat, `*❃ ──────⊰ ❀ ⊱────── ❃*\n*خلص الوقت*\n*الجواب :* *( ${json.name} )*\n*❃ ──────⊰ ❀ ⊱────── ❃*`, conn.tebaklagu[id][0])
+            delete conn.tebaklagu[id]
         }, timeout)
     ]
 }
 handler.help = ['guessflag']
 handler.tags = ['game']
-handler.command = /^guessflag/i
+handler.command = /^علم/i
 
 export default handler
-
