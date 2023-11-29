bfd4b47f87a95921322e8f1f0a81eb8ff92f15aa
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-11-30 00:53:17 +0300

    Update game-guessFlag_Ans.js

--- plugins/game-guessFlag_Ans.js
+++ plugins/game-guessFlag_Ans.js
@@ -2,31 +2,30 @@ import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/use.*fhint/i.test(m.quoted.text) || /.*hhint/i.test(m.text))
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/ماهي.*الدولة/i.test(m.quoted.text) || /.*hhint/i.test(m.text))
         return !0
-    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
-    if (!(id in this.tebakbendera))
-        return this.reply(m.chat, 'The game was over', m)
-    if (m.quoted.id == this.tebakbendera[id][0].id) {
-        let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
+    this.tebaklagu = this.tebaklagu ? this.tebaklagu : {}
+    if (!(id in this.tebaklagu))
+        return this.reply(m.chat, '*خلصت*', m)
+    if (m.quoted.id == this.tebaklagu[id][0].id) {
+        let isSurrender = /^(انسحب|surr?ender)$/i.test(m.text)
         if (isSurrender) {
-            clearTimeout(this.tebakbendera[id][3])
-            delete this.tebakbendera[id]
-            return this.reply(m.chat, '*Yeah, give up :( !*', m)
+            clearTimeout(this.tebaklagu[id][3])
+            delete this.tebaklagu[id]
+            return this.reply(m.chat, '*ماااش مافي مستوى*', m)
         }
-        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
-        
+        let json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]))
+
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
-            this.reply(m.chat, `✅ *correct!*\n+${this.tebakbendera[id][2]} XP`, m)
-            clearTimeout(this.tebakbendera[id][3])
-            delete this.tebakbendera[id]
+            global.db.data.users[m.sender].exp += this.tebaklagu[id][2]
+            this.reply(m.chat, `*❃ ──────⊰ ❀ ⊱────── ❃*\n*❀ شوكولولو ❀*\n\n*◍ الجائزة :* *${this.tebaklagu[id][2]}* *بيلي*\n*❃ ──────⊰ ❀ ⊱────── ❃*`, m)
+            clearTimeout(this.tebaklagu[id][3])
+            delete this.tebaklagu[id]
         } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
-            m.reply(`❗ *A Little More!*`)
+            m.reply(`*اوخخ قربتت*`)
         else
-            this.reply(m.chat, `❌ *Wrong!*`, m)
+            this.reply(m.chat, `*نااه*`, m)
     }
     return !0
 }
 export const exp = 0
-
