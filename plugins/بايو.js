2b92504180f4610e56725d0fa05aa9f536235f61
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-11-30 04:23:20 +0300

    Update and rename Shubham-getbio.js to بايو.js

--- plugins/بايو.js
+++ plugins/بايو.js
@@ -6,18 +6,18 @@ let handler = async (m, { conn, text, command }) => {
     let bio = await conn.fetchStatus(who)
     m.reply(bio.status)
   } catch {
-    if (text) throw `bio is private!`
+    if (text) throw `*البايو برايفت*`
     else try {
       let who = m.quoted ? m.quoted.sender : m.sender
       let bio = await conn.fetchStatus(who)
       m.reply(bio.status)
     } catch {
-      throw `bio is private!`
+      throw `*البايو برايفت*`
     }
   }
 }
 handler.help = ['getbio <@tag/reply>']
 handler.tags = ['group']
-handler.command = /^(getb?io)$/i
+handler.command = /^(بايو)$/i
 handler.limit = true
 export default handler
