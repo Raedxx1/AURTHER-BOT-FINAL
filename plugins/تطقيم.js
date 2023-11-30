917b0200b38c315dc04549c7c8374b07dfd7ce59
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-11-30 04:29:46 +0300

    Update and rename Wall-couplepp.js to تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn }) => {
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['img']
-handler.command = ['couplepp','ppcouple'] 
+handler.command = ['طقم','تطقيم'] 
 
 
 export default handler
