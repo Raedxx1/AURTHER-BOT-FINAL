b1b0f8310b66f8bcc9a39bfbdd07f655a1fa8524
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-01 03:05:38 +0300

    Update econ-levelup.js

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -1,10 +1,33 @@
 import { canLevelUp, xpRange } from '../lib/levelling.js'
+import Canvacord from 'canvacord';
 
 let handler = async (m, { conn }) => {
-let { role } = global.db.data.users[m.sender]
-let name = conn.getName(m.sender)
- let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Menu2.jpg')
+let { exp, level, role } = global.db.data.users[who];
+let { min, xp } = xpRange(user.level, global.multiplier);
+let name = conn.getName(who);
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+ 
+ if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`;
+
+let crxp = exp - min
+let customBackground  = './Assets/rankbg.jpg'
+let requiredXpToLevelUp = xp
+ const card = await new Canvacord.Rank()
+  .setAvatar(pp)
+  .setLevel(level)
+  .setCurrentXP(crxp) 
+  .setRequiredXP(requiredXpToLevelUp) 
+  .setProgressBar('#db190b', 'COLOR') // Set progress bar color here
+  .setDiscriminator(who.substring(3, 7))
+  .setCustomStatusColor('#db190b')
+  .setLevelColor('#FFFFFF', '#FFFFFF')
+  .setOverlay('#000000')
+  .setUsername(username)
+  .setBackground('IMAGE', customBackground)
+  .setRank(level, 'LEVEL', false)
+  .renderEmojis(true)
+  .build();
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg');
 let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
@@ -17,8 +40,12 @@ let user = global.db.data.users[m.sender]
 *-انت تحتاج ${max - user.exp} من  اكس بي للوصول الى مستوى التالي⏳*
 *╾───━⛱️━───╼*
 `
-conn.sendFile(m.chat, pp, 'levelup.jpg', lvl, m)
-    }
+try {
+     conn.sendFile(m.chat, card, 'rank.jpg', str, m, false, { mentions: [who] });
+    m.react('✅');
+        } catch (e) {
+            m.reply(txt)
+        }
 
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
@@ -32,9 +59,9 @@ conn.sendFile(m.chat, pp, 'levelup.jpg', lvl, m)
 *╾───━⛱️━───╼*
 `
           .trim()
-        try {
-            const img = await levelup(teks, user.level)
-            conn.sendFile(m.chat, pp, 'levelup.jpg', str, m)
+       try {
+    conn.sendFile(m.chat, card, 'rank.jpg', str, m, false, { mentions: [who] });
+    m.react('✅');
         } catch (e) {
             m.reply(str)
         }
