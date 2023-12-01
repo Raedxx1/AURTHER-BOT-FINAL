2b7c99cea75627632aa938b8ae5f9fc0dfaf7177
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-01 23:50:15 +0300

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,17 +2,17 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `✳️ *Enter serial number*\nCheck your serial number with the command...\n\n*${usedPrefix}nserie*`
+  if (!args[0]) throw `*أكتب الأيدي الخاص بك ... لمعرفة الأيدي أكتب ( .ايدي )*`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw '⚠️ *Incorrect serial number*'
+  if (args[0] !== sn) throw '⚠️ *الأيدي خطأ*'
   user.registered = false
-  m.reply(`✅ Register eliminated`)
+  m.reply(`✅ *تم تسجيل الخروج*`)
 }
 handler.help = ['unreg <Num Serie>'] 
 handler.tags = ['rg']
 
-handler.command = ['unreg'] 
+handler.command = ['خروج'] 
 handler.register = true
 
 export default handler
