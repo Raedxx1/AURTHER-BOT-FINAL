5f6b39be957e7e8450ed5806bf1e7e9de65ff9ef
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-01 23:43:04 +0300

    Update rg-sn.js

--- plugins/rg-sn.js
+++ plugins/rg-sn.js
@@ -2,12 +2,13 @@ import { createHash } from 'crypto'
 
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
-m.reply(`
-▢ *seriel number* : ${sn}
+m.reply(`*❃ ──────⊰ ❀ ⊱────── ❃*
+◍ *الأيدي* : ${sn}
+*❃ ──────⊰ ❀ ⊱────── ❃*
 `.trim())
 }
 handler.help = ['mysn']
 handler.tags = ['rg']
-handler.command = ['nserie', 'sn', 'mysn'] 
+handler.command = ['ايدي', 'sn', 'mysn'] 
 handler.register = true
 export default handler
