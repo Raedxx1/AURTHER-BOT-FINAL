3c20160bcc3c620f16f851a2e6a38669fc6bf4ec
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-01 05:17:40 +0300

    Update econ-levelup.js

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -3,43 +3,38 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 let handler = async (m, { conn }) => {
 let { role } = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
-let background = 'https://i.ibb.co/4YBNyvP/images-76.jpg'
+ let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Menu2.jpg')
 let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
-      let lvl = `*╾───━⛱️━───╼*
-*┇ 『الإسـم╎ 🪪⌞ ${name}』*
-*┇ 『المستوى╎ 📊⌞ ${user.level}』*
-*┇ 『التصنيف╎ 🎭⌞ ${role}』*
-*┇ 『 اكس بي╎ 🔢⌞ ${user.exp - min}/${xp}』*
-*╾───━⛱️━───╼*
-*-انت تحتاج ${max - user.exp} من  اكس بي للوصول الى مستوى التالي⏳*
-*╾───━⛱️━───╼*
+      let lvl = `*❃ ──────⊰ ❀ ⊱────── ❃*
+◍ *الأسم :*  *${name}*
+◍ *المستوى :*  *${user.level}*
+◍ *الخبرة :* *${user.exp - min}/${xp}*
+◍ *التصنيف :* *${user.role}*
+*❃ ──────⊰ ❀ ⊱────── ❃*
+
+*مرحبا* *${name}* *لايمكنك الترقي حاليا انت تحتاج الى* *${max - user.exp}* *للصعود للمستوى التالي*
 `
-try {
-            let imgg = `https://wecomeapi.onrender.com/rankup-image?username=${encodeURIComponent(name)}&currxp=${user.exp - min}&needxp=${xp}&level=${user.level}&rank=${encodeURIComponent(pp)}&avatar=${encodeURIComponent(pp)}&background=${encodeURIComponent(background)}`
-            conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
-        } catch (e) {
-            m.reply(lvl)
+conn.sendFile(m.chat, pp, 'levelup.jpg', lvl, m)
     }
 
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
     if (before !== user.level) {
-        let teks = `عاش يحب! ${conn.getName(m.sender)} المستوى: ${user.level}`
+        let teks = `تست ${conn.getName(m.sender)} المستوى: ${user.level}`
         let str = `
-*╾───━⛱️━───╼*
-*┇『المستـوى السابق╎ 📈⌞ ${before}』*
-*┇『المستـوى الحالي╎ 📊⌞ ${user.level}』*
-*┇『التصنيف╎ ♟️⌞ ${role}』*
-*╾───━⛱️━───╼*
+*❃ ──────⊰ ❀ ⊱────── ❃*
+◍ *المستوى السابق :* *${user.level - 1}*
+◍ *المستوى الحالي :* *${user.level}*
+◍ *التصنيف :* *${user.role}*
+*❃ ──────⊰ ❀ ⊱────── ❃*
 `
           .trim()
         try {
-            let img = `https://wecomeapi.onrender.com/levelup-image?avatar=${encodeURIComponent(pp)}`
-            conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+            const img = await levelup(teks, user.level)
+            conn.sendFile(m.chat, pp, 'levelup.jpg', str, m)
         } catch (e) {
             m.reply(str)
         }
@@ -49,7 +44,7 @@ await delay(5 * 5000)
 handler.help = ['levelup']
 handler.tags = ['xp']
 
-handler.command = ['nivel', 'lvl', 'رانك', 'لفل'] 
+handler.command = ['nivel', 'lvl', 'لفل'] 
 
 export default handler
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
