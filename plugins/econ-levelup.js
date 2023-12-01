c2fbda4487bd8e2b88be7e9217d65c446818de14
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-01 04:37:52 +0300

    Update econ-levelup.js

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -1,53 +1,55 @@
-import { canLevelUp, xpRange } from '../lib/levelling.js';
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 
 let handler = async (m, { conn }) => {
-    let name = conn.getName(m.sender);
-    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg');
-    let user = global.db.data.users[m.sender];
-    let background = 'https://i.ibb.co/4YBNyvP/images-76.jpg'; // Fixed background URL
-
+let { role } = global.db.data.users[m.sender]
+let name = conn.getName(m.sender)
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
+let background = 'https://i.ibb.co/4YBNyvP/images-76.jpg'
+let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-        let { min, xp, max } = xpRange(user.level, global.multiplier);
-        let txt = `
-┌───⊷ *LEVEL*
-▢ Number : *${name}*
-▢ Level : *${user.level}*
-▢ XP : *${user.exp - min}/${xp}*
-▢ Role : *${user.role}*
-└──────────────
-
-Hey there, ${name}! You're not ready to level up just yet. It seems like you need to munch up *${max - user.exp}* more XP to level up and reach new heights! Keep going, and the bots will be singing your praises soon! 🚀
-`.trim();
-
-        try {
-            let imgg = `https://wecomeapi.onrender.com/rankup-image?username=${encodeURIComponent(name)}&currxp=${user.exp - min}&needxp=${xp}&level=${user.level}&rank=${encodeURIComponent(pp)}&avatar=${encodeURIComponent(pp)}&background=${encodeURIComponent(background)}`;
-            conn.sendFile(m.chat, imgg, 'level.jpg', txt, m);
+        let { min, xp, max } = xpRange(user.level, global.multiplier)
+      let lvl = `*╾───━⛱️━───╼*
+*┇ 『الإسـم╎ 🪪⌞ ${name}』*
+*┇ 『المستوى╎ 📊⌞ ${user.level}』*
+*┇ 『التصنيف╎ 🎭⌞ ${role}』*
+*┇ 『 اكس بي╎ 🔢⌞ ${user.exp - min}/${xp}』*
+*╾───━⛱️━───╼*
+*-انت تحتاج ${max - user.exp} من  اكس بي للوصول الى مستوى التالي⏳*
+*╾───━⛱️━───╼*
+`
+try {
+            let imgg = `https://wecomeapi.onrender.com/rankup-image?username=${encodeURIComponent(name)}&currxp=${user.exp - min}&needxp=${xp}&level=${user.level}&rank=${encodeURIComponent(pp)}&avatar=${encodeURIComponent(pp)}&background=${encodeURIComponent(background)}`
+            conn.sendFile(m.chat, imgg, 'level.jpg', txt, m)
         } catch (e) {
-            m.reply(txt);
-        }
-    } else {
+            m.reply(lvl)
+    }
+
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+    if (before !== user.level) {
+        let teks = `عاش يحب! ${conn.getName(m.sender)} المستوى: ${user.level}`
         let str = `
-┌─⊷ *LEVEL UP*
-▢ Previous level : *${user.level - 1}*
-▢ Current level : *${user.level}*
-▢ Role : *${user.role}*
-└──────────────
-
-Woo-hoo, ${name}! You've soared to new heights and reached level ${user.level}! 🎉 Time to celebrate! 🎊
-Your newfound power will strike fear into the hearts of trolls, and the bots will bow before your command! Keep up the incredible work, and who knows what epic adventures await you next! 🌟
-`.trim();
-
+*╾───━⛱️━───╼*
+*┇『المستـوى السابق╎ 📈⌞ ${before}』*
+*┇『المستـوى الحالي╎ 📊⌞ ${user.level}』*
+*┇『التصنيف╎ ♟️⌞ ${role}』*
+*╾───━⛱️━───╼*
+`
+          .trim()
         try {
-            let img = `https://wecomeapi.onrender.com/levelup-image?avatar=${encodeURIComponent(pp)}`;
-            conn.sendFile(m.chat, img, 'levelup.jpg', str, m);
+            let img = `https://wecomeapi.onrender.com/levelup-image?avatar=${encodeURIComponent(pp)}`
+            conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
         } catch (e) {
-            m.reply(str);
+            m.reply(str)
         }
     }
+await delay(5 * 5000)  
 }
+handler.help = ['levelup']
+handler.tags = ['xp']
 
-handler.help = ['levelup'];
-handler.tags = ['economy'];
-handler.command = ['lvl', 'levelup', 'level'];
+handler.command = ['nivel', 'lvl', 'رانك', 'لفل'] 
 
 export default handler
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
