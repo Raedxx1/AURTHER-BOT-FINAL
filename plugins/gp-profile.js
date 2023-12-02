8a3840e7e9ad56f3de077785f3c84b66a01b9271
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-03 02:51:01 +0300

    Update gp-profile.js

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -1,45 +1,54 @@
-import { createHash } from 'crypto'
-import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { xpRange, canLevelUp } from '../lib/levelling.js';
+import { createHash } from 'crypto';
+import Canvacord from 'canvacord';
 
-let handler = async (m, { conn, usedPrefix, command}) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  
+  if (!(who in global.db.data.users)) throw '✳️ المستخدم غير موجود ف قاعدة بياناتي';
 
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ المستخدم غير موجود ف قاعدة بياناتي`
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
-let user = global.db.data.users[who]
-let about = (await conn.fetchStatus(who).catch(console.error) || {}).status || ''
-let { name, exp, credit, lastclaim, registered, regTime, age, level, role, wealth, warn } = global.db.data.users[who]
-let { min, xp, max } = xpRange(user.level, global.multiplier)
-let username = conn.getName(who)
-let math = max - xp 
-let prem = global.prems.includes(who.split`@`[0])
-let sn = createHash('md5').update(who).digest('hex')
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg');
+  let user = global.db.data.users[who];
+  let about = (await conn.fetchStatus(who).catch(console.error))?.status || '';
+  let { name, exp, credit, lastclaim, registered, regTime, age, level, role, wealth, warn } = global.db.data.users[who];
+  let { min, xp, max } = xpRange(user.level, global.multiplier);
+  let username = conn.getName(who);
+  let math = max - xp;
+  let prem = global.prems.includes(who.split@[0]);
+  let sn = createHash('md5').update(who).digest('hex');
+  let customBackground = './Assets/rankbg.jpg';
 
-// • @${who.replace(/@.+/, '')}
-let str = ` *❃ ──────⊰ ❀ ⊱────── ❃*\n
-*🪪 الأسم:* ${username}${about ? '\n\n 🎌 *الوصف:* ' + about : ''}
+  let crxp = exp - min;
+  let requiredXpToLevelUp = xp;
 
-*⚠️ الأنذارات:* ${warn}/${maxwarn}
+  const card = await new Canvacord.Rank()
+    .setAvatar(pp)
+    .setLevel(level)
+    .setCurrentXP(crxp) 
+    .setRequiredXP(requiredXpToLevelUp) 
+    .setProgressBar('#e1d4a7', 'COLOR') // Set progress bar color here
+    .setDiscriminator(who.substring(3, 7))
+    .setCustomStatusColor('#e1d4a7')
+    .setLevelColor('#FFFFFF', '#FFFFFF')
+    .setOverlay('#000000')
+    .setUsername(username)
+    .setBackground('IMAGE', customBackground)
+    .setRank(level, 'LEVEL', false)
+    .renderEmojis(true)
+    .build();
 
-*💰 الرصيد :* ${credit} *بيلي*
+  const str = *❃ ──────⊰ ❀ ⊱────── ❃*\n\n*🪪 الأسم:* ${username}${about ? '\n\n 🎌 *الوصف:* ' + about : ''}\n\n*⚠️ الأنذارات:* ${warn}/${maxwarn}\n\n*💰 الرصيد :* ${credit} *بيلي*\n\n*✨ المستوى :* ${level}\n\n*⬆️ الخبره :* (${user.exp - min} / ${xp})\n${math <= 0 ? 'أنت مستعد للترقي' : ''}\n\n*🏆 التصنيف :* ${role}\n\n*📇 الحساب :* ${registered ? 'مسحل' : 'غير مسجل'}\n\n*⭐️ العضوية :*  ${prem ? 'مميز' : 'عضو'}\n\n*❃ ──────⊰ ❀ ⊱────── ❃*;
 
-*✨ المستوى :* ${level}
+  try {
+    conn.sendFile(m.chat, card, 'rank.jpg', str, m, false, { mentions: [who] });
+    m.react('✅');
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-*⬆️ الخبره :* (${user.exp - min} / ${xp})\n${math <= 0 ? `*أنت مستعد للترقي*`}
+handler.help = ['combinedProfileRank'];
+handler.tags = ['economy', 'group'];
+handler.command = ['حسابي-رانك'];
 
-*🏆 التصنيف :* ${role}
-
-*📇 الحساب :* ${registered ? 'مسحل': 'غير مسجل'}
-
-*⭐ العضوية :*  ${prem ? 'مميز' : 'عضو'}\n
- *❃ ──────⊰ ❀ ⊱────── ❃*
-`
-    conn.sendFile(m.chat, pp, 'profil.jpg', str, m, false, { mentions: [who] })
-    m.react(done)
-
-}
-handler.help = ['profile']
-handler.tags = ['group']
-handler.command = ['حسابي','بروفايل'] 
-
-export default handler
+export default handler;
