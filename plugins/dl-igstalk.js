e7380d322249e9ca70743fe56b5a1bbd4fc85453
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-04 01:48:55 +0300

    Update dl-igstalk.js

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -2,25 +2,25 @@
 import fg from 'api-dylux'
 let handler= async (m, { conn, args, text, usedPrefix, command }) => {
 	
-    if (!args[0]) throw `✳️ Enter the Instagram Username\n\n📌Example: ${usedPrefix + command} asli_guru69` 
+    if (!args[0]) throw `جيب يوز انستا\n\nمثال : ${usedPrefix + command} أي يوزر تبيه` 
     let res = await fg.igStalk(args[0])
     let te = `
-┌──「 *STALKING* 
-▢ *🔖Number:* ${res.name} 
-▢ *🔖Username:* ${res.username}
-▢ *👥followers:* ${res.followersH}
-▢ *🫂following:* ${res.followingH}
-▢ *📌Bio:* ${res.description}
-▢ *🏝️Posts:* ${res.postsH}
+*❃ ──────⊰ ❀ ⊱────── ❃*
+◍ *الأسم :* ${res.name} 
+◍ *اليوزر :* ${res.username}
+◍ *المتابعين :* ${res.followersH}
+◍ *يتابع :* ${res.followingH}
+◍ *البايو :* ${res.description}
+◍ *البوستات:* ${res.postsH}
 
-▢ *🔗 Link* : https://instagram.com/${res.username.replace(/^@/, '')}
-└────────────`
+▢ *🔗 الرابط* : https://instagram.com/${res.username.replace(/^@/, '')}
+*❃ ──────⊰ ❀ ⊱────── ❃*`
 
      await conn.sendFile(m.chat, res.profilePic, 'tt.png', te, m)
      
 }
 handler.help = ['igstalk']
 handler.tags = ['downloader']
-handler.command = ['igstalk'] 
+handler.command = ['انستجرام'] 
 
 export default handler
