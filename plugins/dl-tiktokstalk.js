24ff83e19cca1584e7679523f013f58079b321e3
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-04 02:08:48 +0300

    Update dl-tiktokstalk.js

--- plugins/dl-tiktokstalk.js
+++ plugins/dl-tiktokstalk.js
@@ -2,22 +2,23 @@
 import fg from 'api-dylux'
 let handler = async (m, { conn, text, args }) => {
 	
-  if (!text) throw `✳️Enter the Username of a TikTok user `
+  if (!text) throw `عطني يوز المستخدم الي بتبحث عليه`
   let res = await fg.ttStalk(args[0])
   let txt = `
-┌──「 *TIKTOK STALK* 
-▢ *🔖Number:* ${res.name}
-▢ *🔖Username:* ${res.username}
-▢ *👥followers:* ${res.followers}
-▢ *🫂following:* ${res.following}
-▢ *📌Desc:* ${res.desc}
+*❃ ──────⊰ ❀ ⊱────── ❃*
+◍ *الأسم :* ${res.name} 
+◍ *اليوزر :* ${res.username}
+◍ *المتابعين :* ${res.followers}
+◍ *يتابع :* ${res.following}
+◍ *الوصف :* ${res.desc}
 
-▢ *🔗 Link* : https://tiktok.com/${res.username}
-└────────────`
+▢ *🔗 الرابط* : https://tiktok.com/${res.username}
+*❃ ──────⊰ ❀ ⊱────── ❃*`
+
   await conn.sendFile(m.chat, res.profile, 'tt.png', txt, m)
 }
 handler.help = ['tiktokstalk']
 handler.tags = ['downloader']
-handler.command = /^t(tstalk|iktokstalk)$/i
+handler.command = /^تيكتوك$/i
 
 export default handler
