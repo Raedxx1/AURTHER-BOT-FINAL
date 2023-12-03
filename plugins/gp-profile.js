fb1af2ebaeecea08bdb11f051e00d5d00ac96019
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-03 03:06:47 +0300

    Update gp-profile.js

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -1,54 +1,53 @@
-import { xpRange, canLevelUp } from '../lib/levelling.js';
 import { createHash } from 'crypto';
+import { canLevelUp, xpRange } from '../lib/levelling.js';
 import Canvacord from 'canvacord';
 
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  
-  if (!(who in global.db.data.users)) throw '✳️ المستخدم غير موجود ف قاعدة بياناتي';
+
+  if (!(who in global.db.data.users)) throw ✳️ اهذا المستخدم غير موجود ف قاعدة بياناتي;
 
   let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg');
   let user = global.db.data.users[who];
-  let about = (await conn.fetchStatus(who).catch(console.error))?.status || '';
+  let about = (await conn.fetchStatus(who).catch(console.error) || {}).status || '';
   let { name, exp, credit, lastclaim, registered, regTime, age, level, role, wealth, warn } = global.db.data.users[who];
-  let { min, xp, max } = xpRange(user.level, global.multiplier);
+  let { min, xp } = xpRange(user.level, global.multiplier);
   let username = conn.getName(who);
-  let math = max - xp;
-  let prem = global.prems.includes(who.split@[0]);
-  let sn = createHash('md5').update(who).digest('hex');
-  let customBackground = './Assets/rankbg.jpg';
-
-  let crxp = exp - min;
-  let requiredXpToLevelUp = xp;
+  
+  let math = max - xp 
+  let prem = global.prems.includes(who.split`@`[0])
+  let sn = createHash('md5').update(who).digest('hex')
+  let crxp = exp - min
+  let customBackground  = './Assets/rankbg.jpg'
+  let requiredXpToLevelUp = xp
 
   const card = await new Canvacord.Rank()
-    .setAvatar(pp)
-    .setLevel(level)
-    .setCurrentXP(crxp) 
-    .setRequiredXP(requiredXpToLevelUp) 
-    .setProgressBar('#e1d4a7', 'COLOR') // Set progress bar color here
-    .setDiscriminator(who.substring(3, 7))
-    .setCustomStatusColor('#e1d4a7')
-    .setLevelColor('#FFFFFF', '#FFFFFF')
-    .setOverlay('#000000')
-    .setUsername(username)
-    .setBackground('IMAGE', customBackground)
-    .setRank(level, 'LEVEL', false)
-    .renderEmojis(true)
-    .build();
+  .setAvatar(pp)
+  .setLevel(level)
+  .setCurrentXP(crxp) 
+  .setRequiredXP(requiredXpToLevelUp) 
+  .setProgressBar('#e1d4a7', 'COLOR') // Set progress bar color here
+  .setDiscriminator(who.substring(3, 7))
+  .setCustomStatusColor('#e1d4a7')
+  .setLevelColor('#FFFFFF', '#FFFFFF')
+  .setOverlay('#000000')
+  .setUsername(username)
+  .setBackground('IMAGE', customBackground)
+  .setRank(level, 'LEVEL', false)
+  .renderEmojis(true)
+  .build();
 
-  const str = *❃ ──────⊰ ❀ ⊱────── ❃*\n\n*🪪 الأسم:* ${username}${about ? '\n\n 🎌 *الوصف:* ' + about : ''}\n\n*⚠️ الأنذارات:* ${warn}/${maxwarn}\n\n*💰 الرصيد :* ${credit} *بيلي*\n\n*✨ المستوى :* ${level}\n\n*⬆️ الخبره :* (${user.exp - min} / ${xp})\n${math <= 0 ? 'أنت مستعد للترقي' : ''}\n\n*🏆 التصنيف :* ${role}\n\n*📇 الحساب :* ${registered ? 'مسحل' : 'غير مسجل'}\n\n*⭐️ العضوية :*  ${prem ? 'مميز' : 'عضو'}\n\n*❃ ──────⊰ ❀ ⊱────── ❃*;
+  const str = *❃ ──────⊰ ❀ ⊱────── ❃*\n\n◍ *الأسم :* ${username}\n\n◍ *الخبره :* ${crxp} / ${requiredXpToLevelUp}\n\n◍ *التصنيف :* *${role}*\n\n *❃ ──────⊰ ❀ ⊱────── ❃*
 
   try {
     conn.sendFile(m.chat, card, 'rank.jpg', str, m, false, { mentions: [who] });
     m.react('✅');
   } catch (error) {
     console.error(error);
-  }
-};
+  }}
 
-handler.help = ['combinedProfileRank'];
-handler.tags = ['economy', 'group'];
-handler.command = ['حسابي-رانك'];
+handler.help = ['rank'];
+handler.tags = ['economy'];
+handler.command = ['بروفايل'];
 
 export default handler;
