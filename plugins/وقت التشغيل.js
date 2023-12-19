95e388ea5ef3c9d8264fe31458d38cbf3eeaaee9
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-19 11:10:18 +0300

    Update and rename main-runtime.js to وقت التشغيل.js

--- plugins/وقت التشغيل.js
+++ plugins/وقت التشغيل.js
@@ -10,11 +10,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-   m.reply(`🏮 *Bot active duration*  \n\n${muptime}`) 
+   m.reply(`🏮 *البوت شغال لمدة :*  \n\n${muptime}`) 
 }
 handler.help = ['runtime']
 handler.tags = ['main']
-handler.command = ['runtime', 'uptime']
+handler.command = ['وقت التشغيل']
 export default handler
 
 function clockString(ms) {
