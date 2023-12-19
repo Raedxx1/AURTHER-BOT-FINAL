cbf9ec1929f3a9c2549b899b2181cc68f3c3bd10
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-19 11:05:52 +0300

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,23 +1,33 @@
-import {
-  promises,
-  readFileSync
- } from "fs"
- import {
-  join
- } from "path"
- import {
-  xpRange
- } from "../lib/levelling.js"
- import moment from "moment-timezone"
- import os from "os"
- import fs from "fs"
- import fetch from "node-fetch"
- 
- let str = `
-  *%ucpn*
-  *❃ ──────⊰ ❀ ⊱────── ❃*
+import fetch from 'node-fetch';
+import { xpRange } from '../lib/levelling.js';
+import { promises,readFileSync } from 'fs';
+import moment from 'moment-timezone';
+import { join } from 'path';
+import os from 'os';
+import fs from 'fs';
+
+    let handler = async (m, { conn, usedPrefix, command}) => {
+  try {
+    let vn = 'https://i.imgur.com/ey6rk2T.mp4'; // Video URL
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? 
+    conn.user.jid : m.sender;
+    if (!(who in global.db.data.users)) throw `✳️ هذا المستخدم غير موجود في قاعدة بياناتي`;
+    let user = global.db.data.users[who];
+    let about = (await conn.fetchStatus(who).catch(console.error) || {}).status || '';
+    let { name, exp, credit, lastclaim, registered, regTime, age, level, role, wealth, warn } = 
+    global.db.data.users[who];
+    let { min, xp, max } = xpRange(user.level, global.multiplier);
+    let username = conn.getName(who);
+    let math = max - xp;
+
+    
+
+    // Menu text
+    let str = `
+*❃ ──────⊰ ❀ ⊱────── ❃*
                       *الـمـجـمـوعـات*
 *❃ ──────⊰ ❀ ⊱────── ❃* 
+
   ◍ منشن
   ◍ مخفي
   ◍ طرد
@@ -32,9 +42,11 @@ import {
   ◍ طرد_رمز
   ◍ انذار
   ◍ الغاء_الانذار 
+  
 *❃ ──────⊰ ❀ ⊱────── ❃*
                           *الفعاليات*
 *❃ ──────⊰ ❀ ⊱────── ❃* 
+
   ◍ ش
   ◍ سؤال
   ◍ عين
@@ -44,9 +56,11 @@ import {
   ◍ كت
   ◍ تف
   ◍ تر 
+  
 *❃ ──────⊰ ❀ ⊱────── ❃*
                     *الحياة الافتراضية*
 *❃ ──────⊰ ❀ ⊱────── ❃* 
+
   ◍ المحفظة
   ◍ البنك
   ◍ المتجر 
@@ -66,9 +80,11 @@ import {
   ◍ عمل
   ◍ المتصدرين
   ◍ منح 
+  
 *❃ ──────⊰ ❀ ⊱────── ❃*
                         *الـتـحـمـيـل*
 *❃ ──────⊰ ❀ ⊱────── ❃* 
+
   ◍ فيديو
   ◍ اغنيه
   ◍ صورة
@@ -76,9 +92,11 @@ import {
   ◍ فيديو
   ◍ البحث
   ◍ تيك 
+  
 *❃ ──────⊰ ❀ ⊱────── ❃*
                           *الـتـرفـيـه*
 *❃ ──────⊰ ❀ ⊱────── ❃* 
+
   ◍ اكس_او
   ◍ احذف_اللعبه
   ◍ تحدي
@@ -96,9 +114,11 @@ import {
   ◍ شطرنج
   ◍ سلاحي
   ◍ نصايح 
+  
 *❃ ──────⊰ ❀ ⊱────── ❃*
                          *الـتـحـويـل*
-*❃ ──────⊰ ❀ ⊱────── ❃* 
+*❃ ──────⊰ ❀ ⊱────── ❃*
+
   ◍ ملصق
   ◍ حقوق
   ◍ لصورة
@@ -109,9 +129,11 @@ import {
   ◍ كلب
   ◍ ستك
   ◍ مترجم 
+  
 *❃ ──────⊰ ❀ ⊱────── ❃*
                           *الاعـضـاء*
 *❃ ──────⊰ ❀ ⊱────── ❃* 
+
   ◍ اختفاء
   ◍ تحدث
   ◍ سرعه
@@ -124,9 +146,11 @@ import {
   ◍ ايات
   ◍ المطور
   ◍ جوجل 
+  
 *❃ ──────⊰ ❀ ⊱────── ❃*
-                             *آرثر*
+                              *آرثر*
 *❃ ──────⊰ ❀ ⊱────── ❃* 
+
   ◍ بان
   ◍ بان_فك
   ◍ بان_شات
@@ -134,172 +158,30 @@ import {
   ◍ حطها_بروفايل
   ◍ اخرج
   ◍ ادخل
+  
 *❃ ──────⊰ ❀ ⊱────── ❃*
- `.trimstr(),
- }
-  
- let handler = async (m, {
-  conn,
-  usedPrefix: _p,
-  __dirname,
-  args
- }) => {
-  await conn.sendMessage(m.chat, {
-   react: {
- text: "⏳",
- key: m.key,
-   }
-  })
-  
-  let tags = {}
-  
-  try {
-  
-   /* Info Menu */
-   let glb = global.db.data.users
-   let usrs = glb[m.sender]
-   let tag = `@${m.sender.split("@")[0]}`
-   let mode = global.opts["self"] ? "Private" : "Public"
-   let _package = JSON.parse(await promises.readFile(join(__dirname, "../package.json")).catch(_ => ({}))) || {}
-   let {
- age,
- exp,
- limit,
- level,
- role,
- registered,
- credit
-   } = glb[m.sender]
-   let {
- min,
- xp,
- max
-   } = xpRange(level, global.multiplier)
-   let name = await conn.getName(m.sender)
-   let premium = glb[m.sender].premiumTime
-   let prems = `${premium > 0 ? "Premium": "Free"}`
-   let platform = os.platform()
- 
- 
-   let ucpn = `${ucapan()}`
-  
-   let _uptime = process.uptime() * 1000
-   let _muptime
-   if (process.send) {
- process.send("uptime")
- _muptime = await new Promise(resolve => {
-  process.once("message", resolve)
-  setTimeout(resolve, 1000)
- }) * 1000
-   }
-   let muptime = clockString(_muptime)
-   let uptime = clockString(_uptime)
- 
+    `.trim();
+
    
-   let totalfeatures = Object.values(global.plugins).filter((v) => v.help && v.tags).length;
-   let totalreg = Object.keys(glb).length
-   let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
- return {
-  help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
-  tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
-  prefix: "customPrefix" in plugin,
-  limit: plugin.limit,
-  premium: plugin.premium,
-  enabled: !plugin.disabled,
- }
-   })
-   let text = typeof conn.menu == "string" ? conn.menu : typeof conn.menu == "object" ? _text : ""
-   let replace = {
- "%": "%",
- p: _p,
- uptime,
- muptime,
- me: conn.getName(conn.user.jid),
- npmname: _package.name,
- npmdesc: _package.description,
- version: _package.version,
- exp: exp - min,
- maxexp: xp,
- totalexp: exp,
- xp4levelup: max - exp,
- github: _package.homepage ? _package.homepage.url || _package.homepage : "[unknown github url]",
- tag,
- ucpn,
- platform,
- mode,
- _p,
- credit,
- age,
- tag,
- name,
- prems,
- level,
- limit,
- name,
- totalreg,
- totalfeatures,
- role,
- readmore: readMore
-   }
-   text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, "g"), (_, name) => "" + replace[name])
-   const pp = './Assets/Gurulogo.jpg'
-  
- 
- let contact = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '212684151146@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
- 
-  conn.sendMessage(m.chat, { video: { url: menuvid }, caption: text.trim(),  gifPlayback: true,
-  gifAttribution: 0}, { quoted: contact })
+    //let messageOptions = {
+      //text: str
+    //};
 
-  } catch (e) {
-   await conn.reply(m.chat, " error", m)
-   throw e
-  }
- }
- handler.command = /^(منيوو|منيو|\?)$/i
- 
+    // Sending the menu text
+    //conn.sendMessage(m.chat, messageOptions, 'extendedTextMessage', {
+      //quoted: m,
+      //contextInfo: { mentionedJid: [m.sender] }
+    //});
 
- 
- export default handler
- 
- 
- function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
- }
- 
- const more = String.fromCharCode(8206)
- const readMore = more.repeat(4001)
- 
- function clockString(ms) {
-  let h = isNaN(ms) ? "--" : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? "--" : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? "--" : Math.floor(ms / 1000) % 60
-  return [h, " H ", m, " M ", s, " S "].map(v => v.toString().padStart(2, 0)).join("")
- }
- 
- function clockStringP(ms) {
-  let ye = isNaN(ms) ? "--" : Math.floor(ms / 31104000000) % 10
-  let mo = isNaN(ms) ? "--" : Math.floor(ms / 2592000000) % 12
-  let d = isNaN(ms) ? "--" : Math.floor(ms / 86400000) % 30
-  let h = isNaN(ms) ? "--" : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? "--" : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? "--" : Math.floor(ms / 1000) % 60
-  return [ye, " *سنوات 🗓️*\n", mo, " *اشهر 🌙*\n", d, " *ايام ☀️*\n", h, " *ساعات 🕐*\n", m, " *دقائق ⏰*\n", s, " *ثواني ⏱️*"].map(v => v.toString().padStart(2, 0)).join("")
- }
- 
- function ucapan() {
-  const time = moment.tz("Asia/Kolkata").format("HH")
-  let res = "*اوخخ ع الصبحية الحلوة ذي ☀️*"
-  if (time >= 4) {
-   res = "*صباح الخير 🌄*"
+    // Sending the video
+    conn.sendMessage(m.chat, { video: { url: vn }, caption: str.trim(), gifPlayback: true, gifAttribution: 0 }, { quoted: m });
+  } catch {
+    conn.reply(m.chat, '*هناك خطأ*', m);
   }
-  if (time >= 10) {
-   res = "*الظاهر الظهريه حلوه ☀️*"
-  }
-  if (time >= 15) {
-   res = "*صباح العصر 🌇*"
-  }
-  if (time >= 18) {
-   res = "*تصبحون ع خير 🌙*"
-  }
-  return res
- }
+};
+
+handler.command = /^(اوامر)$/i;
+handler.exp = 50;
+handler.fail = null;
+
+export default handler;
