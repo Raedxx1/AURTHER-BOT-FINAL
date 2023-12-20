e240678a3a10b7c92706b71344a7628aa3781724
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-20 19:38:01 +0300

    Update econ-deposit.js

--- plugins/econ-deposit.js
+++ plugins/econ-deposit.js
@@ -1,17 +1,17 @@
 const xpperbank = 1
 let handler = async (m, { conn, command, args }) => {
-    let count = command.replace(/^(dep|deposit)$/i, '')
+    let count = command.replace(/^(ايداع|إيداع)$/i, '')
     count = count ? /depall/i.test(count) ? Math.floor(global.db.data.users[m.sender].credit / xpperbank) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
     count = Math.max(1, count)
     if (global.db.data.users[m.sender].credit >= xpperbank * count) {
       global.db.data.users[m.sender].credit -= xpperbank * count
       global.db.data.users[m.sender].bank += count
-      conn.reply(m.chat, `You have transferred 🪙 ${count} gold to your bank`, m)
-    } else conn.reply(m.chat, `🟥 *You don't have sufficient amount of gold in your wallet to make this transaction*`, m)
+      conn.reply(m.chat, `*تم إيداع* 🪙 ${count} *إلى حسابك*`, m)
+    } else conn.reply(m.chat, `🟥 *رصيدك غير كافي*`, m)
   }
   handler.help = ['deposit']
   handler.tags = ['economy']
-  handler.command = ['deposit', 'dep', 'depall'] 
+  handler.command = ['إيداع', 'ايداع', 'وديعه'] 
   
   handler.disabled = false
   
