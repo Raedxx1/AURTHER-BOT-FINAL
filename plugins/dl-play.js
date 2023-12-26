39be3da81672929d801a74222df1059293f91a6c
AURTHER <67609334+kimos71@users.noreply.github.com>
2023-12-26 19:45:36 +0300

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -68,7 +68,7 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
 
 handler.help = ['play'].map((v) => v + ' <query>');
 handler.tags = ['downloader'];
-handler.command = ['أغنية', 'شغل']
+//handler.command = ['أغنية', 'شغل']
 
 handler.exp = 0;
 handler.diamond = false;
