4eeb0b6d644b13c8eff783228a2265c99d700b30
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-01-07 01:37:08 +0300

    Update منشن ارثر.js

--- plugins/منشن ارثر.js
+++ plugins/منشن ارثر.js
@@ -1,52 +1,66 @@
-//Guru ka Maal Hai 
-//Made For Guru Bot
-//created on Diwali(12th Nov)
-//copy with credits
-//lodusheks stay away
+import fetch from 'node-fetch';
 
+// List of three MP3 URLs
+const mp3URLs = [
+  "https://dl.sndup.net/mb86/AUD-20231225-WA0684.mp3",
+  "https://dl.sndup.net/kcb6/AUD-20231229-WA0719.mp3",
+  "https://dl.sndup.net/htb6/AUD-20231230-WA0712.mp3",
+  "https://dl.sndup.net/k7z3/grsgtgv.mp3"
+];
 
+function getRandomMP3URL() {
+  // Generate a random index within the range of the mp3URLs array
+  const randomIndex = Math.floor(Math.random() * mp3URLs.length);
+  // Return the randomly selected MP3 URL
+  return mp3URLs[randomIndex];
+}
 
-let handler = m => m
-handler.all = async function (m, conn) {
-    var vn = "https://github.com/kimos71/AURTHER-BOT-FINAL/raw/main/Assets/mp3/Guru1.mp3"
-    let url = "https://wa.me/212684151146"
-    let murl = "https://www.instagram.com"
-    let hash = global.botname
-    let img = "https://telegra.ph/file/ffe1bb51bfa1b5c224bc9.jpg"
-    let num = "212684151146"
+let handler = async function(m, conn) {
+  try {
+    // Fetch a random MP3 URL
+    const vn = getRandomMP3URL();
+
+    // Rest of your handler code remains the same
+    let url = "https://wa.me/212684151146";
+    let murl = "https://www.instagram.com";
+    let hash = global.botname;
+    let img = "https://telegra.ph/file/ffe1bb51bfa1b5c224bc9.jpg";
+    let num = "212684151146";
 
     let doc = {
-        audio: {
-          url: vn
-        },
-        mimetype: 'audio/mpeg',
-        ptt: true,
-        waveform: [0,99,0,99,0,99,0],
-        fileName: "AURTHER",
-    
-        contextInfo: {
-          mentionedJid: [m.sender],
-          externalAdReply: {
+      audio: {
+        url: vn
+      },
+      mimetype: 'audio/mpeg',
+      ptt: true,
+      waveform: [0, 99, 0, 99, 0, 99, 0],
+      fileName: "AURTHER",
+      contextInfo: {
+        mentionedJid: [m.sender],
+        externalAdReply: {
           title: "↺ |◁   II   ▷|   ♡",
           body: hash,
           thumbnailUrl: img,
           sourceUrl: url,
           mediaType: 2,
           mediaUrl: murl,
-         // renderLargerThumbnail: true,
           showAdAttribution: true
-          }}
-      };
-	
-    let phoneNumber = '';
-    if (m.mentionedJid && m.mentionedJid[0]) {
-        phoneNumber = m.mentionedJid[0].replace(/[^0-9]/g, '');
-        if (phoneNumber === num) {
-          return this.sendMessage(m.chat, doc, { quoted: m });
         }
-      } else {
-        return
       }
-}
-export default handler
+    };
 
+    // Sending the audio message with context information
+    this.sendMessage(m.chat, doc, { quoted: m });
+  } catch (error) {
+    console.error('Handler error:', error);
+    // Handle the error or send an error message back
+    this.sendMessage(m.chat, 'There was an error. Please try again later.', { quoted: m });
+  }
+};
+
+handler.all = handler.command = handler.regex = async function(m, conn) {
+  // Ensure the handler is triggered properly
+  await handler(m, conn);
+};
+
+export default handler;
