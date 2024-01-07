75083bcc6057251d0ad40c6da452aebefcd7e10b
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-01-08 01:28:33 +0300

    Update rg-register.js

--- plugins/rg-register.js
+++ plugins/rg-register.js
@@ -1,30 +1,30 @@
-//import db from '../lib/database.js'
-
 import { createHash } from 'crypto'
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
   if (user.registered === true) throw `*لقد تم تسجيلك ب الفعل*`
-  if (!Reg.test(text)) throw `*مثال : .تسجيل اسمك.عمرك*`
+  if (!Reg.test(text)) throw `*مثال : .تسجيل ( اللقب )*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '*الأسم لا يمكن أن يكون فارغ*'
-  if (!age) throw '*العمر لا يمكن أن يكون فارغ*'
-  if (name.length >= 30) throw '*الأسم طويل*' 
-  age = parseInt(age)
-  if (age > 100) throw '*استهبال هو ؟؟ تست رح تاخذ باند من البوت*'
-  if (age < 5) throw '🚼*دزمها يا بزر وش جابك هنا*'
+  if (!name) throw '*اللقب لا يمكن أن يكون فارغ*'
+  if (!age) {
+    // If age is not provided, set it to a default value (e.g., 0)
+    age = 0
+  } else {
+    if (age.length >= 30) throw '*اللقب طويل*' 
+    age = parseInt(age)
+    if (age > 100) throw '*استهبال هو ؟؟ تست رح تاخذ باند من البوت*'
+    if (age < 15) throw '🚼*دزمها يا بزر وش جابك هنا*'
+  }
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`*❃ ──────⊰ ❀ ⊱────── ❃*
-◍ *تم تسجيلك في قاعدة البيانات*
+◍ *تم تسجيل اللقب*
 *❃ ──────⊰ ❀ ⊱────── ❃*
-◍ *الأسم :* *${name}*
-◍ *العمر :* *${age}* 
-◍ *الايدي :* *${sn}*
+◍ *اللقب :* *${name}*
 *❃ ──────⊰ ❀ ⊱────── ❃*
 `.trim())
 }
@@ -34,4 +34,3 @@ handler.tags = ['rg']
 handler.command = ['تسجيل', 'اشتراك', 'register', 'registrar'] 
 
 export default handler
-
