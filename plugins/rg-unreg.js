114302253a8a17c651376b4b227c104915f0b79c
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-01-08 04:51:22 +0300

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,18 +2,22 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `*أكتب الأيدي الخاص بك ... لمعرفة الأيدي أكتب ( .ايدي )*`
-  let user = global.db.data.users[m.sender]
-  let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw '⚠️ *الأيدي خطأ*'
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  let user = global.db.data.users[who];
+  let { name } = global.db.data.users[who];
+  if (!args[0]) throw `*أكتب اللقب الي تبي تحذفه من قاعدة البيانات*`
+  if (args[0] !== name) throw '⚠️ *تمت ازالتة مسبقا*'
   user.registered = false
-  m.reply(`✅ *تم تسجيل الخروج*`)
+  m.reply(`✅ *تم حذف اللقب*`)
 }
 handler.help = ['unreg <Num Serie>'] 
 handler.tags = ['rg']
 
-handler.command = ['خروج'] 
+handler.command = ['ازالة'] 
 handler.register = true
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
 
 export default handler
-
