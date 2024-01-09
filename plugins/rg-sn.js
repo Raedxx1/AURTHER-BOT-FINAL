56ca59b25703242795e812ea9a1ff794ac85b643
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-01-09 03:38:08 +0300

    Update rg-sn.js

--- plugins/rg-sn.js
+++ plugins/rg-sn.js
@@ -1,15 +1,30 @@
-import {createHash} from 'crypto';
-  const handler = async (m, { conn, args, groupMetadata}) => {
-   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-   if (!(who in global.db.data.users)) throw `المستخدم غير موجود ف قاعدة بياناتي`
+import { createHash } from 'crypto';
+
+const handler = async (m, { conn, args, groupMetadata }) => {
+  let who =
+    m.quoted?.sender ||
+    (m.mentionedJid && m.mentionedJid[0]) ||
+    (m.fromMe ? conn.user.jid : m.sender);
+
+  if (!(who in global.db.data.users)) {
+    throw 'المستخدم غير موجود في قاعدة البيانات';
+  }
+
   const user = global.db.data.users[who];
-  const { name } = global.db.data.users[who];
-  m.reply(`*❃ ──────⊰ ❀ ⊱────── ❃*
-                             *${name}*
-*❃ ──────⊰ ❀ ⊱────── ❃*`.trim());
+  const { name, registered } = user;
+
+  if (registered) {
+    m.reply(`*❃ ──────⊰ ❀ ⊱────── ❃*\n\n                     *${name}*\n
+*❃ ──────⊰ ❀ ⊱────── ❃*`.trim()
+    );
+  } else {
+    m.reply('*هذا المستخدم لم يتم تسجيله بعد .*');
+  }
 };
+
 handler.help = ['myns'];
 handler.tags = ['xp'];
 handler.command = /^(لقبه|الاسم)$/i;
 handler.register = true;
+
 export default handler;
