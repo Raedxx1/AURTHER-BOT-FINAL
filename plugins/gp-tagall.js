18e4ca46bc5129a05d90a7dbf706c2ada367df9c
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-01-16 05:44:27 +0300

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,14 +1,38 @@
-let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
-    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ الأسم : *${groupMetadata.subject}*\n▢ الأعضاء : *${participants.length}*${text ? `\n▢ الرسالة : ${text}\n` : ''}\n*❃ ──────⊰ ❀ ⊱────── ❃*n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n*❃ ──────⊰ ❀ ⊱────── ❃*', null, {
-        mentions: users
-    })
+let handler = async (m, { conn, participants }) => {
+  let teks = "*❃ ──────⊰ ❀ ⊱────── ❃* \n\n *مـنشــــــن عـــــــــام لأعــــــضاء ومشرفـــين*\n *كـــــوكـــــــب 〖 𝑼𝑹𝑨𝑵𝑼𝑺 ¦ 銀河💠 〗*\n *المنشن خاص للمشرفين نتأسف على الازعاج* \n\n";
+  teks += "*❃ ──────⊰ ⚠️ ⊱────── ❃*\n\n"; // Add your custom text here
+
+  // Sort participants based on names
+  participants.sort((a, b) => {
+    let userA = global.db.data.users[a.id] || { registered: false, name: "غير مسجل ⚠️" };
+    let userB = global.db.data.users[b.id] || { registered: false, name: "غير مسجل ⚠️" };
+    return userA.name.localeCompare(userB.name, 'ar', { sensitivity: 'base' });
+  });
+
+  let currentLetter = '';
+
+  for (let mem of participants) {
+    let user = global.db.data.users[mem.id] || { registered: false, name: "غير مسجل ⚠️" };
+    let firstLetter = user.name.charAt(0);
+
+    if (firstLetter !== currentLetter) {
+      // Start a new section for the current letter
+      teks += `*❃ ──────⊰ ${firstLetter} ⊱────── ❃*\n\n`;
+      currentLetter = firstLetter;
+    }
+
+    teks += `◍ ${user.registered ? user.name : "غير مسجل ⚠️"} @${mem.id.split('@')[0]}\n\n`;
+  }
+
+  teks += "*❃ ──────⊰ ❀ ⊱────── ❃*";
+
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) });
 }
 
-handler.help = ['tagall']
-handler.tags = ['group']
-handler.command = ['منشن']
-handler.admin = true
-handler.group = true
+handler.help = ['mentionall'];
+handler.tags = ['group'];
+handler.command = /^منشن$/i;
+handler.group = true;
+handler.admin = true;
 
-export default handler
+export default handler;
