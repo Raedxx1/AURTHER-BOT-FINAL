0b9abcf74e62927252243f4c8ec8cea673c33598
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-01-16 04:24:17 +0300

    Update الالقاب.js

--- plugins/الالقاب.js
+++ plugins/الالقاب.js
@@ -1,34 +1,49 @@
 let handler = async function (m, { conn, args }) {
-  let registeredNames = Object.values(global.db.data.users)
-    .filter(user => user.registered && user.name.match(/[\u0600-\u06FF]/)) // Filter only names with Arabic characters
+  let groupInfo = await conn.groupMetadata(m.chat);
+
+  if (!groupInfo) {
+    return m.reply('حدث خطأ في الحصول على معلومات المجموعة.');
+  }
+
+  let groupMembers = groupInfo.participants;
+  let registeredUsers = Object.values(global.db.data.users)
+    .filter(user => user.registered && user.name.match(/[\u0600-\u06FF]/))
     .map(user => user.name)
     .sort((a, b) => a.localeCompare(b, 'ar'))
 
-  if (registeredNames.length === 0) {
-    return m.reply('لا يوجد أسماء مسجلة بالحروف العربية في قاعدة البيانات.')
+  if (registeredUsers.length === 0) {
+    return m.reply('لم يتم تسجيل اي لقب بعد');
   }
 
-  let namesList = '*❃ ──────⊰ ❀ ⊱────── ❃*\n                    *الألقاب المأخوذة* '
+  let namesList = '*❃ ──────⊰ ❀ ⊱────── ❃*\n                    *الألقاب المأخوذة* ';
+  let currentLetter = '';
 
-  let currentLetter = ''
-  for (let name of registeredNames) {
-    let firstLetter = name.charAt(0)
+  for (let name of registeredUsers) {
+    let firstLetter = name.charAt(0);
     if (firstLetter !== currentLetter) {
-      namesList += `\n*❃ ──────⊰ ${firstLetter} ⊱────── ❃*\n\n`
-      currentLetter = firstLetter
+      namesList += `\n*❃ ──────⊰ ${firstLetter} ⊱────── ❃*\n\n`;
+      currentLetter = firstLetter;
     }
-    namesList += `◍ *${name}* \n`
+    namesList += `◍ *${name}* \n`;
   }
 
-  conn.reply(m.chat, namesList.trim(), m)
+  let registeredCount = registeredUsers.length;
+  let totalMembers = groupMembers.length;
+
+  namesList += `\n*❃ ──────⊰ ❀ ⊱────── ❃*\n`;
+  namesList += (registeredCount === totalMembers) ? '◍ *تم تسجيل جميع الأعضاء*\n' : `عدد المسجلين: *${registeredCount} / ${totalMembers}*\n`;
+  namesList += '*❃ ──────⊰ ❀ ⊱────── ❃*'; // Added line
+
+
+  conn.reply(m.chat, namesList.trim(), m);
 }
 
-handler.help = ['قائمة']
-handler.tags = ['قائمة', 'أسماء', 'عربية']
-handler.command = ['الألقاب', 'الالقاب' ,'~']
-handler.group = true
-handler.admin = false
-handler.botAdmin = false
-handler.fail = null
+handler.help = ['قائمة'];
+handler.tags = ['قائمة', 'أسماء', 'عربية'];
+handler.command = ['القاب', 'الألقاب', 'الالقاب'];
+handler.group = true;
+handler.admin = false;
+handler.botAdmin = false;
+handler.fail = null;
 
-export default handler
+export default handler;
