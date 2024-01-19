7eee2af9735d7468656caf6b735eb6773b90f003
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-01-19 04:12:18 +0300

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,5 +1,5 @@
 import { spawn } from 'child_process'
-let handler = async (m, { conn,  text }) => {
+let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
     await m.reply('🔄 تست...\n اصبر شويتين')
@@ -11,7 +11,6 @@ handler.help = ['restart']
 handler.tags = ['owner']
 handler.command = ['ريستارت'] 
 
-handler.admin = true;
-handler.botAdmin = true;
+handler.rowner = true
 
 export default handler
