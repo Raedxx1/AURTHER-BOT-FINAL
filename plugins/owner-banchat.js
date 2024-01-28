5b65021ac948e5bb5c38591e47c81a61b9f6f7a8
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-01-29 00:45:08 +0300

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,7 +1,7 @@
 //import db from '../lib/database.js'
 
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
-    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
     m.reply('*تم*')
 }
