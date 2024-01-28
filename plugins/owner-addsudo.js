88ba652051dbad68d9436d89ecc9111b09566b46
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-01-29 00:17:37 +0300

    Update owner-addsudo.js

--- plugins/owner-addsudo.js
+++ plugins/owner-addsudo.js
@@ -1,22 +1,22 @@
 let handler = async (m, { conn, text }) => {
-    let who;
-    if (m.isGroup) {
-        who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
-    } else {
-        who = m.chat;
-    }
-    let name = await conn.getName(m.quoted.sender)
-    if (!who) throw 'Tag the person you want to make an Owner!';
-    if (global.owner.includes(who.split('@')[0])) throw 'This person is already an owner!';
-    global.owner.push([who.split('@')[0], name, true]);
-    const caption = `Now @${who.split('@')[0]} has been made an Owner!`;
-    await conn.reply(m.chat, caption, m, {
-        mentions: conn.parseMention(caption)
-    });
+  let who;
+  if (m.isGroup) {
+      who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
+  } else {
+      who = m.chat;
+  }
+  if (!who) throw '*منشن شخص*';
+  if (global.owner.includes(who.split('@')[0])) throw 'لقد أصبح مشرفا ب الفعل !';
+  global.owner.push([who.split('@')[0], m.name, true]);
+  const caption = `*مبروك @${who.split('@')[0]} لقد أصبحت مشرفا*`;
+  await conn.reply(m.chat, caption, m, {
+      mentions: conn.parseMention(caption)
+  });
 }
-handler.help = ['addowner @user']
+handler.help = ['sudo @user']
 handler.tags = ['owner']
-handler.command = /^(add|give|-)(owner|sudo)$/i;
-handler.owner = true
+handler.command = /^مشرف/i;
+handler.admin = true
+handler.group = true
 
 export default handler;
