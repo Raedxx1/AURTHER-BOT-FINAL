033d808e38500e5d449a1c02d4163e7da400d176
AURTHER <67609334+kimos71@users.noreply.github.com>
2024-02-23 20:30:27 +0300

    Update gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -13,7 +13,7 @@ m.reply(`*تـــم الــطرد !*`)
 
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = ['اتوكل', 'كلبطه', 'اخرس','طرد','دزمها''احبك','اكرهك',] 
+handler.command = ['اتوكل','كلبطه','اخرس','طرد','دزمها','احبك','اكرهك'] 
 handler.owner = true
 handler.group = true
 handler.botAdmin = true
